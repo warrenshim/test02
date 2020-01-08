70a0988668e8cdf60592d131e696a35f238419da
warrenshim <warrenshim@gmail.com>
2020-01-08 15:06:12 +0900

    Update i18n_data.py

--- attendance_management_bot/model/i18n_data.py
+++ attendance_management_bot/model/i18n_data.py
@@ -92,7 +92,7 @@ def get_i18n_content_by_lang(fmt, local, lang, **kw):
         date: Local time of datetime object.
     :return: a string.
     """
-    local_map = {'en': 'en_US', 'ja': 'ja_JP', 'kr': 'ko_KR'}
+    local_map = {'en': 'en_US', 'ja': 'ja_JP', 'ko': 'ko_KR'}
     local_text = gettext.translation(local, 'locales', [lang])
 
     date = None
